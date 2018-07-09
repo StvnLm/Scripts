56e7ef6fc9031dbb15399d6552efa83489a01982
Steven Lam <34800247+StvnLm@users.noreply.github.com>
2018-07-09 11:36:41 -0400

    Update SLA.py

--- SLA_Checker/SLA.py
+++ SLA_Checker/SLA.py
@@ -1,16 +1,13 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QComboBox, QPushButton, QGridLayout, QLabel, QSizePolicy, QGroupBox, QVBoxLayout, QDateTimeEdit, QMessageBox, QMainWindow, QCheckBox
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import QDateTime, Qt
+from PyQt5 import QtCore
 
 from datetime import date
 import sys, json
 from Program import *
 from ValidDate import *
-
-'''
-TODO: Comment code
-'''
-
+import Program
 
 class popup(QWidget):
     def __init__(self, name):
@@ -18,6 +15,8 @@ class popup(QWidget):
 
 
 class HolidayWindow(QWidget):
+
+    checked_list = []
 
     def __init__(self):
         super().__init__()
@@ -29,6 +28,8 @@ class HolidayWindow(QWidget):
         vboxLayout = QVBoxLayout()
         vboxLayout.addWidget(self.groupBox)
         self.setLayout(vboxLayout)
+        # self.check_counter = 0
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.show()
 
     def centre(self):
@@ -38,6 +39,15 @@ class HolidayWindow(QWidget):
         window.moveCenter(centre)
         self.move(window.topLeft())
 
+    def tally_holidays(self, checkbox_list):
+        check_counter = 0
+        for checkbox in checkbox_list:
+            if checkbox.isChecked():
+                check_counter  += 1
+        Interface.holiday_cnt = check_counter
+        SLA.SLA_holiday_cnt = check_counter
+        self.close()
+
     def initUI(self):
         self.groupBox = QGroupBox()
         gridLayout = QGridLayout()
@@ -45,7 +55,7 @@ class HolidayWindow(QWidget):
         text_label = QLabel('Select all statutory and provincial holidays that resulted in a business day turning \n'
                             'into a non-working day. Check holidays as they apply to the client local province.\n'
                             'The below displays the corresponding month, applicable prov/territory and holiday name.\n\n'
-                            'Do NOT check if the statutory holiday falls on a Saturday or Sunday as this will skew calculations! ')
+                            'Do NOT check if the statutory holiday occurs on a Saturday or Sunday as this will skew calculations! ')
 
         text_label.setAlignment(Qt.AlignCenter)
         text_label.setFont(QFont("OPEN SANS", 8))
@@ -75,13 +85,21 @@ class HolidayWindow(QWidget):
         gridLayout.addWidget(text_label)
         labels = [QLabel(label) for label in label_list]
         # [label.setFont(QFont("OPEN SANS", 9)) for label in labels]
+
+        checkbox_list = []
         for n in range(len(labels)):
-            cb = gridLayout.addWidget(QCheckBox(label_list[n]), n+1, 0)
+            cb = QCheckBox(label_list[n])
+            gridLayout.addWidget(cb, n+1, 0)
+            checkbox_list.append(cb)
+
 
         confirm = QPushButton('Confirm')
-        confirm.setStyleSheet('QPushButton { border-style: outset; border-width: 1px; border-radius: 3px; border-color: black; min-width: 10em; padding: 5px}'
+        confirm.setStyleSheet('QPushButton {border-style: outset; border-width: 1px; border-radius: 3px; border-color: black; min-width: 10em; padding: 5px}'
                              'QPushButton:hover { background-color: rgba(255, 255, 255, 175) }'
                              'QPushButton:pressed {  background-color: rgba(255, 255, 255, 245) }')
+
+        confirm.clicked.connect(lambda: self.tally_holidays(checkbox_list))
+
         gridLayout.addWidget(confirm)
 
         self.groupBox.setLayout(gridLayout)
@@ -90,6 +108,8 @@ class HolidayWindow(QWidget):
 
 
 class Interface(QWidget):
+
+    holiday_cnt = 0
 
     def __init__(self):
         super().__init__()
@@ -101,6 +121,7 @@ class Interface(QWidget):
         vboxLayout = QVBoxLayout()
         vboxLayout.addWidget(self.groupBox)
         self.setLayout(vboxLayout)
+        holiday_cnt = 0
         self.show()
 
 
@@ -119,13 +140,18 @@ class Interface(QWidget):
             for key in data[client_combo_box.currentText()]['SEV'].keys():
                 sev_combo_box.addItem(key)
 
+    def check_sev(self, sev_combo_box):
+        if sev_combo_box == ('severity 1' or 'severity 2'):
+            self.holiday_button.setEnabled(False)
+        if sev_combo_box == ('severity 3' or 'severity 4'):
+            self.holiday_button.setEnabled(True)
+
     def calculate_press(self, start_datetime, end_datetime, client, sev, prov):
         start_string = start_datetime.dateTime().toString('yyyy M d H m')
         end_string = end_datetime.dateTime().toString('yyyy M d H m')
         st, end = [], []
         [st.append(int(char)) for char in start_string.split(' ')]
         [end.append(int(char)) for char in end_string.split(' ')]
-        # print(st, '\n', end)
         ticket_info = SLA(client=client, severity=sev, st_year=st[0], st_month=st[1], st_day=st[2], st_hr=st[3], st_min=st[4],
                          end_year=end[0], end_month=end[1], end_day=end[2], end_hr=end[3], end_min=end[4])
         ticket_hours = ticket_info.calculate_ticket_hrs()
@@ -137,8 +163,8 @@ class Interface(QWidget):
         else:
             sla_check = f'SLA not breached.'
         # Check for holidays
-        start = datetime.date(st[0], st[1], st[3])
-        end = datetime.date(end[0], end[1], end[2])
+        # start = datetime.date(st[0], st[1], st[3])
+        # end = datetime.date(end[0], end[1], end[2])
         # holiday_list = ValidDate().holidaycheck(start, end, prov)
         # Create Pop up window
         msg = QMessageBox()
@@ -171,9 +197,9 @@ class Interface(QWidget):
         # Buttons
         calculate_button = QPushButton("Calculate")
         calculate_button.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
-        holiday_button = QPushButton("Holidays")
-        holiday_button.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
-        for button in [holiday_button, calculate_button]:
+        self.holiday_button = QPushButton("Holidays")
+        self.holiday_button.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        for button in [self.holiday_button, calculate_button]:
             button.setStyleSheet('QPushButton { border-style: outset; border-width: 1px; border-radius: 3px; border-color: black; min-width: 10em; padding: 3px}'
                                  'QPushButton:hover { background-color: rgba(255, 255, 255, 175) }'
                                  'QPushButton:pressed {  background-color: rgba(255, 255, 255, 245) }')
@@ -198,21 +224,22 @@ class Interface(QWidget):
             for key in data["AC"]['SEV'].keys():
                     sev_combo_box.addItem(key)
             prov = data[client_combo_box.currentText()]['prov']
+            self.holiday_button.setEnabled(False)
 
         # Change sev combo box when client changes
         client_combo_box.currentIndexChanged.connect(lambda: self.repopulateCombo(sev_combo_box, client_combo_box))
+        sev_combo_box.currentIndexChanged.connect(lambda: self.check_sev(sev_combo_box.currentText()))
         # Calculate on button press
-        # holi_window = self.holiday_press()
-
-        holiday_button.clicked.connect(lambda: self.holiday_press())
+        self.holiday_button.clicked.connect(lambda: self.holiday_press())
         calculate_button.clicked.connect(lambda: self.calculate_press(start_date_edit, end_date_edit, client_combo_box.currentText(), sev_combo_box.currentText(), prov))
+
 
         # Add to layout
         gridLayout.addWidget(start_date_edit, 0, 2)
         gridLayout.addWidget(end_date_edit, 1, 2)
         gridLayout.addWidget(client_combo_box,2, 2)
         gridLayout.addWidget(sev_combo_box,3, 2)
-        gridLayout.addWidget(holiday_button, 4, 2)
+        gridLayout.addWidget(self.holiday_button, 4, 2)
         gridLayout.addWidget(calculate_button, 5, 2)
 
         self.groupBox.setLayout(gridLayout)
