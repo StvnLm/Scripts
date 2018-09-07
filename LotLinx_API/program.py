cd701c2f997305266ccbc5aa3309ffbd98f61e5b
Steven Lam <34800247+StvnLm@users.noreply.github.com>
2018-09-06 23:01:53 -0400

    Update program.py

--- LotLinx_API/program.py
+++ LotLinx_API/program.py
@@ -1,22 +1,17 @@
 """"
 TODO:
 - Basic error handling should be implemented, but no heroics necessary.
-- The URLs for images to be processed should be inputs to the program, as arguments, stdin or read from a file.
+- The URLs for images to be processed should be inputs to the program, as
+  arguments, stdin or read from a file.
 - API responses should be logged
-- Provide a ZIP/archive of the program code, logged output and resulting images downloaded from the API results.
+- Provide a ZIP/archive of the program code, logged output and resulting images
+  downloaded from the API results.
 """
 
-from logging import FileHandler
+import credentials #credential.py to store api user/pass
 import requests
 import logging
-import credentials
-
-
-# Init Logging
-logger = logging.getLogger("LotLinxAPI")
-filehandle = logging.FileHandler("LogLinx.log")
-format = logging.Formatter("%(asctime)")
-logger.addHandler(filehandle)
+import time
 
 # API Authentication credentials
 username = credentials.username
@@ -26,64 +21,118 @@ session.auth = (username, password)
 
 base_url = "https://photoai.lotlinx.com"
 
+logging.basicConfig(filename=f"LogLinx.log", level=logging.INFO,
+                    format="%(asctime)s | %(levelname)s | %(message)s")
+
 
 # 1) Submit Requests
 def PostRequest(dealerId, vehicleId, imageId, imageUrl):
-    endpoint = base_url + "/images/optimize"
-    data = [{
-            "dealerId": dealerId,
-            "vehicles": [{
-                        "id": vehicleId,
-                        "images": [{
-                                    "imageId": imageId,
-                                    "imageUrl": imageUrl
-                                  }]
-                        }]
-            }]
-    resp = session.post(url=endpoint, json=data)
-    resp.raise_for_status()
-    return resp
+    try:
+        endpoint = base_url + "/images/optimize"
+        data = [{
+                "dealerId": dealerId,
+                "vehicles": [{
+                            "id": vehicleId,
+                            "images": [{
+                                        "imageId": imageId,
+                                        "imageUrl": imageUrl
+                                      }]
+                            }]
+                }]
+        logging.info(msg="POST request for dealerId: {}, vehicleId: {}, imageId:{}, "
+                         "imageUrl: {}".format(dealerId, vehicleId, imageId, imageUrl))
+        resp = session.post(url=endpoint, json=data)
+        # Raise for status in event of non-200 HTTP response
+        resp.raise_for_status()
+        logging.info(msg="POST Request finished with status code {}".format(resp.status_code))
+        return resp
+
+    except requests.exceptions.ConnectionError:
+        logging.error("Could not find server; check network connection.")
+    except Exception as e:
+        logging.error(f"{e}")
 
 
 # 2) GET REQUEST to query using token
 def GetStatus(token):
-    endpoint = base_url + f"/images/{token}/status"
-    resp = session.get(url=endpoint)
-    resp.raise_for_status()
-    return resp
+    try:
+        logging.info(f"GET Request with token: {token}")
+        endpoint = base_url + f"/images/{token}/status"
+        resp = session.get(url=endpoint)
+        resp.raise_for_status()
+        logging.info("GET Request finished with status code {}".format(resp.status_code))
+        return resp
+    except Exception as e:
+        logging.error(f"{e}")
 
 
-# 2b) GET REQUEST to query using specified status and start date
+# 2b) GET REQUEST to query using status and start date
 def GetRequest(status, startDate):
-    endpoint = base_url + f"/images/requests?status={status}&startDate={startDate}"
-    resp = session.get(url=endpoint)
-    resp.raise_for_status()
-    return resp
+    try:
+        logging.info(f"GET Request for status: {status} and startDate: {startDate}")
+        endpoint = base_url + f"/images/requests?status={status}&startDate={startDate}"
+        resp = session.get(url=endpoint)
+        resp.raise_for_status()
+        logging.info("GET Request finished with status code {}".format(resp.status_code))
+        return resp
+    except Exception as e:
+        logging.error(f"{e}")
 
 
 # 3) LOAD RESPONSE using token
 def GetResponse(token):
-    endpoint = base_url + f"/images/{token}"
-    resp = session.get(url=endpoint)
-    resp.raise_for_status()
-    return resp
+    try:
+        logging.info(f"GET request for token: {token}")
+        endpoint = base_url + f"/images/{token}"
+        resp = session.get(url=endpoint)
+        resp.raise_for_status()
+        logging.info("GET Request finished with status code {}".format(resp.status_code))
+        return resp
+    except Exception as e:
+        logging.error(f"{e}")
 
 
 if __name__ == '__main__':
-    # 1) POST
-    post = PostRequest(dealerId=9876655, vehicleId=987676, imageId=76554,
-                       imageUrl="https://img.lotlinx.com/vdn/7416/jeep_wrangler%20unlimited_2014_1C4BJWFG3EL326863_7416_5_339187295.jpg")
-    post = post.json()
-    token = post["data"][0]["token"]
-    token = "51uA18oltVr6S0SdY2EDZhczppOrC4LALCTSyDu8cCslvYVHQLYmwu6hgz2kHTua"
 
-    # 2) GET /w Token
-    getStat = GetStatus(token)
+
+    tokens = []
+    with open("ImageLinks.txt", "r") as f:
+        for line in f:
+            line = line.replace("\n", "")
+            # 1) POST request for images and tokens
+            post = PostRequest(dealerId=9876655, vehicleId=987636, imageId=76554,
+                               imageUrl=line)
+            # Get tokens for all image links
+            tokens.append(post.json()["data"][0]["token"])
+
+    # 2) GET status /w token
+    successfulTokens = []
+    for token in tokens:
+        getStatus = GetStatus(token)
+        print(token, type(token))
+        while True:
+            print(token, type(token), getStatus.json()["data"][0]["status"])
+            getStatus = GetStatus(token)
+            if getStatus.json()["data"][0]["status"] == "completed":
+                successfulTokens.append(token)
+                print('successful!!!!!!', token)
+                break
+            elif getStatus.json()["data"][0]["status"] == "failed":
+                print(type(token))
+                print(f"failed token {token}")
+                logging.error(f"{token} resulted in failed status")
+                break
+            else:
+                print('sleeping for 30')
+                time.sleep(30)
+
     # 2b) GET /w status & startdate
-    getReq = GetRequest("completed", "2018-09-05T01:22:33")
+    getReq = GetRequest("completed", "2018-09-06T22:00:00")
     getReq = getReq.json()
+    # print(getReq)
 
     # 3) GET LOAD RESPONSE
-    load = GetResponse(token)
-    load = load.json()
-    print(load)
+    for succToken in successfulTokens:
+        load = GetResponse(succToken)
+        print(load.json())
+
